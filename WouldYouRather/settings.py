129958f3864097d32034f37a7489f3e7a74aa131
Jean Antoine <curiositylightbulb@gmail.com>
2020-03-19 10:58:58 -0400

    'changed_indexJS'

--- WouldYouRather/settings.py
+++ WouldYouRather/settings.py
@@ -126,9 +126,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, 'media')
+    # '/var/www/static/',
 ]
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'WYR_Queries/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- static/static_files/index.js
+++ static/static_files/index.js
@@ -1,7 +1,9 @@
 
 
 const content_query_child = document.querySelectorAll('.content-query-child')
-//
+
+const list_menu_a = document.querySelectorAll('.list-menu-a')
+
 const background_still = {       
         first_content() {
                 content_query_child[0].style.background = 'rgb(15, 128, 24)';
@@ -11,12 +13,23 @@ const background_still = {
         second_content() {
                 content_query_child[1].style.background = 'rgb(15, 128, 24)';
                 content_query_child[0].style.background = 'rgb(1, 25, 39)';
-        }
+        },
+
+        // color() {
+        //         list_menu_a.style.background = 'rgb(15, 128, 24)'
+        // },
+
 }
 // console.log(content_query_child)
 
 content_query_child[0].addEventListener('click', background_still.first_content);
 content_query_child[1].addEventListener('click', background_still.second_content);
+
+for(let list of list_menu_a){
+        list.addEventListener('click', (e) => {
+                list_menu_a.style.background = 'rgb(15, 128, 24)'
+        })
+}
 
 
         
